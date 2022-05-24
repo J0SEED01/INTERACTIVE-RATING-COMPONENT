79aa149c254d4b0e13587dafea727463832ccedf
jd23011 <josedaniel23011@gmail.com>
2022-05-23 20:56:04 -0400

    practica frontendmentor

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let number = 0
+
+document.addEventListener('click', e => {
+
+    if(e.target.matches('#numbers p')){
+        document.querySelectorAll('#numbers p').forEach(number => {
+            number.classList.remove('number--select')
+        })
+        e.target.classList.add('number--select')
+        number = e.target.id
+    }
+
+    if(e.target.matches('#submit')){
+        document.querySelector('.card--home').style.setProperty('opacity',0)
+        document.querySelector('.card--home').style.setProperty('visibility','hidden')
+        document.querySelector('.card--thank').style.setProperty('opacity',1)
+        document.querySelector('.card--thank').style.setProperty('visibility','visible')    
+        document.querySelector('.card--thank .card__select').textContent = `You selected ${number} out of 5`
+    
+        setTimeout(function() {
+
+            document.querySelector('.card--home').style.setProperty('opacity',1)
+            document.querySelector('.card--home').style.setProperty('visibility','visible')    
+            document.querySelector('.card--thank').style.setProperty('opacity',0)
+            document.querySelector('.card--thank').style.setProperty('visibility','hidden')
+
+        }, 2000)
+
+    }
+})
+
